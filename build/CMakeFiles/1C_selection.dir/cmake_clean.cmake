d34db4088e0450e19474490cf8b5bd4986beea00
Smirnov Vlad <smirnov.vladislav@phystech.edu>
2023-10-20 23:50:13 +0300

    README is added

---
+++ build/CMakeFiles/1C_selection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1C_selection"
+  "1C_selection.pdb"
+  "CMakeFiles/1C_selection.dir/main.cpp.o"
+  "CMakeFiles/1C_selection.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1C_selection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/1C_selection.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1C_selection.
+# This may be replaced when dependencies are built.
